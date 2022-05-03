dabc973f61b3411ecffc1781cf952b3dd5a88453
Scott schnieders <atsimcode@gmail.com>
2022-05-03 00:11:36 -0700

    rofi

--- rc.lua
+++ rc.lua
@@ -276,6 +276,8 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
+    awful.key({ modkey,           }, "e", function () awful.spawn("rofi -show drun") end,
+              {description = "launcher", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
